12e0752d2acff977914aa57109a2859600d871c3
Jannik Arndt <jannik@jannikarndt.de>
2016-10-09 14:24:30 +0200

    Typings reference

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,4 +2,7 @@
 // https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
+/// <reference path="../typings/globals/es6-shim/index.d.ts" />
+/// <reference path="../typings/globals/google.maps/index.d.ts" />
+
 declare var System: any;
